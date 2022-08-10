e24af41987058586e8f6980c861a1d16be6b048b
DeniFer8 <denifer8@hotmail.com>
2022-08-10 14:22:06 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+//import { Button, Icon } from "semantic-ui-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+import Comics from "./pages/Comics";
+import Series from "./pages/Series";
+import Error404 from "./pages/Error404";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <p>Marvel.</p>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="inicio" element={<Home />} />
+              <Route path="series" element={<Series />} />
+              <Route path="comics" element={<Comics />} />
+              <Route path="*" element={<Error404 />} />
+            </Routes>
+            <nav>
+              <Link to="/comics">Comics</Link>
+              <Link to="/series">Series</Link>
+            </nav>
+          </BrowserRouter>
+
+
+        </header>
+
+      </div>
+
+    </>
   );
 }
 
